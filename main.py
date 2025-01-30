4f9de502ee4bdddfaf4d6298f613198efa621e0a
marcurion <marcurion@private>
2025-01-30 16:15:17 +0100

    Testing how quickly resource consumption grows, function with 8 parameters and parameter variance of 18 consumes over 16GB of RAM and is eventually terminated by zsh

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ class SystemUnderTest:
 
     def concat(self, a:str, b:int):
         return f'{a} + {b} = {a + str(b)}'
+
+    def mean(self, a:int, b:int, c:int, d:int):
+        return (a + b + c + d) / 4
+
+    def concat(self, *args):
+        output = ""
+        for arg in args:
+            output += str(arg)
+        return output
--- tests/run_snapshot_coverage.py
+++ tests/run_snapshot_coverage.py
@@ -13,7 +13,13 @@ strings = ["abc","def","ghi"]
 int_x_int = generator.generate_combinations(None, ints, ints)
 ints += [4,5]
 int_x_int = generator.generate_combinations(int_x_int, ints, ints)
-
+ints += [0, -1, 99, 100, -2, -100, 64000]
+int_x_int = generator.generate_combinations(int_x_int, ints, ints)
+int_x4 = generator.generate_combinations(None, ints, ints, ints, ints)
+ints += [42, 77, 71, 9999, 1000000, -7]
+int_x_int = generator.generate_combinations(int_x_int, ints, ints)
+int_x4 = generator.generate_combinations(int_x4, ints, ints, ints, ints)
+int_x8 = generator.generate_combinations(None, ints, ints, ints, ints, ints, ints, ints, ints)
 
 def test_add_function(snapshot):
         for test_tuple in int_x_int:
@@ -25,3 +31,13 @@ def test_subtract_function(snapshot):
         for test_tuple in int_x_int:
             print(test_tuple)
             assert system.sub(*test_tuple) == snapshot
+
+def test_mean_function(snapshot):
+    for test_tuple in int_x4:
+        print(test_tuple)
+        assert system.mean(*test_tuple) == snapshot
+
+def test_concat_function(snapshot):
+    for test_tuple in int_x8:
+        print(test_tuple)
+        assert system.concat(*test_tuple) == snapshot
